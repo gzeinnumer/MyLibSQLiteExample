6228976ae32a32eda2d46abdc64baa1f69234e91
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2020-12-12 15:45:45 +0700

    readme

--- app/src/main/java/com/gzeinnumer/mylibsqliteexample/TestActivity.java
+++ app/src/main/java/com/gzeinnumer/mylibsqliteexample/TestActivity.java
@@ -23,24 +23,37 @@ public class TestActivity extends AppCompatActivity {
 
         Table1 table1 = new Table1();
 
-        List<Table1> list = table1.read();
-        Log.d(TAG, "onCreate1: "+list.get(0).getName());
-        Log.d(TAG, "onCreate1: "+list.size());
+        //Active 1 by 1 one if you want to test it
 
-        List<Table1> listQuery = table1.query();
-        Log.d(TAG, "onCreate2: "+listQuery.get(0).getName());
-        Log.d(TAG, "onCreate2: "+listQuery.get(0).getTable2_name());
+//        boolean istrue = table1.insert();
+//        Log.d(TAG, "onCreate1: "+istrue);
 
-        boolean istrue = table1.insert(new Table1(500, "Zein", 1.3, "Decs", 1,"2020-12-12"));
-        Log.d(TAG, "onCreate3: "+istrue);
+//        boolean istrue2 = table1.update();
+//        Log.d(TAG, "onCreate2: "+istrue2);
 
-        boolean istrue2 = table1.update(new Table1(500, "Zein", 1.6, "Decs", 1,"2020-11-11"));
-        Log.d(TAG, "onCreate4: "+istrue2);
+//        boolean istrue3 = table1.delete();
+//        Log.d(TAG, "onCreate3: "+istrue3);
 
-        int count = table1.count();
-        Log.d(TAG, "onCreate5: "+count);
+//        int count = table1.count();
+//        Log.d(TAG, "onCreate4: "+count);
 
-        boolean istrue3 = table1.delete();
-        Log.d(TAG, "onCreate6: "+istrue3);
+//        int count2 = table1.count2();
+//        Log.d(TAG, "onCreate5: "+count2);
+
+//        List<Table1> read = table1.read();
+//        Log.d(TAG, "onCreate6: "+read.get(0).getName());
+//        Log.d(TAG, "onCreate6: "+read.size());
+
+//        List<Table1> read2 = table1.read2();
+//        Log.d(TAG, "onCreate7: "+read2.get(0).getName());
+//        Log.d(TAG, "onCreate7: "+read2.size());
+
+//        List<Table1> listQuery = table1.query();
+//        Log.d(TAG, "onCreate8: "+listQuery.get(0).getName());
+//        Log.d(TAG, "onCreate8: "+listQuery.get(0).getTable2_name());
+//        Log.d(TAG, "onCreate8: "+listQuery.size());
+
+//        boolean queryUpdate = table1.queryResultUpdate();
+//        Log.d(TAG, "onCreate9: "+queryUpdate);
     }
 }
