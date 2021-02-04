c4bbef5f408e647ab2d181160937c9e51c61f54a
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-02-04 17:49:46 +0700

    read

--- app/src/main/java/com/gzeinnumer/mylibsqliteexample/TestActivity.java
+++ app/src/main/java/com/gzeinnumer/mylibsqliteexample/TestActivity.java
@@ -2,6 +2,7 @@ package com.gzeinnumer.mylibsqliteexample;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -29,47 +30,45 @@ public class TestActivity extends AppCompatActivity {
 
         binding.btnInsert.setOnClickListener(view -> {
             boolean istrue = table1.insert();
-            Log.d(TAG, "onCreate_1: " + istrue);
+            Toast.makeText(this, "Insert data success " + istrue, Toast.LENGTH_SHORT).show();
         });
         binding.btnUpdate.setOnClickListener(view -> {
             boolean istrue2 = table1.update();
-            Log.d(TAG, "onCreate_2: " + istrue2);
+            Toast.makeText(this, "Update data success " + istrue2, Toast.LENGTH_SHORT).show();
         });
         binding.btnDelete.setOnClickListener(view -> {
             boolean istrue3 = table1.delete();
-            Log.d(TAG, "onCreate_3: " + istrue3);
+            Toast.makeText(this, "Delete data success " + istrue3, Toast.LENGTH_SHORT).show();
         });
         binding.btnCount.setOnClickListener(view -> {
             int count = table1.count();
-            Log.d(TAG, "onCreate_4: " + count);
+            Toast.makeText(this, "Count data success " + count, Toast.LENGTH_SHORT).show();
         });
         binding.btnCount2.setOnClickListener(view -> {
             int count2 = table1.count2();
-            Log.d(TAG, "onCreate_5: " + count2);
+            Toast.makeText(this, "Count data success " + count2, Toast.LENGTH_SHORT).show();
         });
         binding.btnCount3.setOnClickListener(view -> {
             int count3 = table1.queryCount();
-            Log.d(TAG, "onCreate_6: " + count3);
+            Toast.makeText(this, "Count data success " + count3, Toast.LENGTH_SHORT).show();
         });
         binding.btnRead.setOnClickListener(view -> {
             List<Table1> read = table1.read();
-            Log.d(TAG, "onCreate_7: " + read.get(0).getName());
-            Log.d(TAG, "onCreate_7: " + read.size());
+            Toast.makeText(this, "Read data success " + read.get(0).getName() + " Size " + read.size(), Toast.LENGTH_SHORT).show();
         });
         binding.btnRead2.setOnClickListener(view -> {
             List<Table1> read2 = table1.read2();
-            Log.d(TAG, "onCreate_8: " + read2.get(0).getName());
-            Log.d(TAG, "onCreate_8: " + read2.size());
+            Toast.makeText(this, "Read data success " + read2.get(0).getName() + " Size " + read2.size(), Toast.LENGTH_SHORT).show();
         });
         binding.btnQuery.setOnClickListener(view -> {
             List<Table1> listQuery = table1.query();
-            Log.d(TAG, "onCreate_9: " + listQuery.get(0).getName());
-            Log.d(TAG, "onCreate_9: " + listQuery.get(0).getTable2_name());
-            Log.d(TAG, "onCreate_9: " + listQuery.size());
+            Toast.makeText(this, "Read data success " + listQuery.get(0).getName() + " Size " + listQuery.size() + " Table2Name " + listQuery.get(0).getTable2_name(), Toast.LENGTH_SHORT).show();
         });
         binding.btnQueryResultUpdate.setOnClickListener(view -> {
             boolean queryUpdate = table1.queryResultUpdate();
             Log.d(TAG, "onCreate_10: " + queryUpdate);
+            Toast.makeText(this, "Update data success " + queryUpdate, Toast.LENGTH_SHORT).show();
+
         });
     }
 }
